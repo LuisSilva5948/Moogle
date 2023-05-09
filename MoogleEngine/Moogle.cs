a738abf5906275d5e6955aa5506b96c898d2606f
Luis Silva <luissilvajr2003@gmail.com>
2023-05-08 21:54:15 -0400

    Comentado

--- MoogleEngine/Moogle.cs
+++ MoogleEngine/Moogle.cs
@@ -5,14 +5,15 @@ namespace MoogleEngine;
 
 public static class Moogle
 {
-	//creando los objetos data y tfidf de sus respectivas clases alisto mis documentos
-	public static DataBase data = new DataBase();
-	public static TFIDF tfidf = new TFIDF(data);
-	public static SearchResult Query(string query) {
+    //creando los objetos data y tfidf de sus respectivas clases alisto mis documentos
+    public static DataBase data = new DataBase();
+    public static TFIDF tfidf = new TFIDF(data);
+    public static SearchResult Query(string query)
+    {
 
         //si la query no es vacia, ejecutar la búsqueda
         if (!string.IsNullOrWhiteSpace(query))
-		{
+        {
             Query user_query = new Query(query, tfidf);
             Similarity similarity = new Similarity(user_query, tfidf);
             SearchItem[] items = similarity.Items.ToArray();
@@ -21,11 +22,11 @@ public static class Moogle
             //los documentos encontrados son devueltos junto con una sugerencia
             return new SearchResult(items, sug);
         }
-		else
-		{
-			SearchItem[] items = new SearchItem[1] 
+        else
+        {
+            SearchItem[] items = new SearchItem[1]
             {new SearchItem("Por favor, ingrese algún término para realizar una búsqueda", "Intente nuevamente", 0)};
-			return new SearchResult(items, "");
-		}
+            return new SearchResult(items, "");
+        }
     }
 }
